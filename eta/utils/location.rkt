e0f813df6255cbf9a5551c799aeafb0a512a7fe3
abap34 <abap0002@gmail.com>
2025-04-12 22:28:16 +0900

    refactor(utils): move `Location` to under utils

---
+++ eta/utils/location.rkt
@@ -0,0 +1,74 @@
+#lang racket
+
+;; Provides location-related functionality for the Eta language
+(provide Location
+         make-location
+         location-equal?
+         Location?
+         Location-sline
+         Location-scol
+         Location-eline
+         Location-ecol
+          location->string
+         create-span-location)
+
+;; Location
+;;    Represents a source code location
+;; Fields:
+;;    sline - Starting line number
+;;    scol - Starting column number
+;;    eline - Ending line number
+;;    ecol - Ending column number
+(struct Location (sline scol eline ecol) #:transparent)
+
+
+;; location->string
+;;    Converts a location to a string representation
+;; Arguments:
+;;    loc - A Location object
+;; Returns:
+;;   A string representation of the location
+(define (location->string loc)
+  (format "(~a, ~a) - (~a, ~a)"
+          (Location-sline loc)
+          (Location-scol loc)
+          (Location-eline loc)
+          (Location-ecol loc)))
+
+
+;; make-location
+;;    Creates a location object for a token
+;; Arguments:
+;;    line - Starting line number
+;;    col - Starting column number
+;;    end-col - Ending column number (same line)
+;; Returns:
+;;    A Location struct
+(define (make-location line col end-col)
+  (Location line col line end-col))
+
+;; location-equal?
+;;    Checks if two locations are equal
+;; Arguments:
+;;    l1 - First location
+;;    l2 - Second location
+;; Returns:
+;;    Boolean indicating if the locations are equal
+(define (location-equal? l1 l2)
+  (and (= (Location-sline l1) (Location-sline l2))
+       (= (Location-scol l1) (Location-scol l2))
+       (= (Location-eline l1) (Location-eline l2))
+       (= (Location-ecol l1) (Location-ecol l2))))
+
+;; create-span-location
+;;    Creates a new location that spans from the start location to the end location
+;; Arguments:
+;;    start-loc - The starting location
+;;    end-loc - The ending location
+;; Returns:
+;;    A new Location that spans from the start to the end
+(define (create-span-location start-loc end-loc)
+  (Location (Location-sline start-loc) 
+            (Location-scol start-loc)
+            (Location-eline end-loc)
+            (Location-ecol end-loc)))
